13bde25b425bb3b839ebe3b353cfc95607222c3f
JoseH-Git <joseh.sierra@outlook.com>
2021-05-28 13:11:12 -0500

    temp update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
+import datetime as dt
 from flask import Flask, jsonify
 
 
@@ -38,11 +38,11 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"//api/v1.0/precipitation<br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start><end>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -51,8 +51,8 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of precipitations"""
-    # Query all passengers
-    results = session.query(measurement.date).all()
+    # Query all precipitation dates
+    results = session.query(measurement.date,measurement.prcp).all()
 
     session.close()
 
@@ -60,6 +60,48 @@ def precipitation():
     all_measurment = list(np.ravel(results))
 
     return jsonify(all_measurment)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of precipitations"""
+    # Query all stations
+    results = session.query(measurement.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+    
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of tobs"""
+    # Query all stations
+    #results = session.query(measurement.tobs,measurement.date).all()
+    last_date= session.query(measurement.date).order_by(measurement.date.desc()).first()
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    print("Query Date: ", query_date)
+    session.close()
+
+    most_active_station = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+    most_active_station
+
+    data_query_temp = session.query(measurement.date, measurement.tobs).filter(measurement.station == 'USC00519281').\
+                  filter(measurement.date >= query_date).order_by(measurement.date.desc()).all()
+    data_query_temp
+
+    # Convert list of tuples into normal list
+    most_active = list(np.ravel(most_active_station))
+    query_temp = list(np.ravel(data_query_temp))
+    #return jsonify(most_active)
+    return jsonify(query_temp)
 
 
 
